82410246f174b86654ee0c820a7a26f644a957f2
Mladen <mladen@example.com>
2025-05-02 19:40:19 +0200

    Setze app.run auf 0.0.0.0:5000, debug=True für Netzwerkbetrieb

--- app.py
+++ app.py
@@ -523,4 +523,4 @@ def background_favorites_updater():
 threading.Thread(target=background_favorites_updater, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True) 
